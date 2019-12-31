d32a8edfd2d84bb48c3dcc5b638dd48eb10a2f75
Joel Junior <joel.asj2@gmail.com>
2019-12-31 18:17:52 -0300

    Initial Commit

---
+++ DynamicList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DynamicList
+//
+//  Created by Joel Júnior on 31/12/19.
+//  Copyright © 2019 jnr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
